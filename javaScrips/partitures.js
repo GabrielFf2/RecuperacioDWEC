07d09692f45a5127f3d3177b23dd65819a366d3d
GabrielFf2 <149058439+GabrielFf2@users.noreply.github.com>
2025-04-03 18:29:12 +0200

    pagina de login feta , no em queda centrada la finestra nova ..

--- javaScrips/partitures.js
+++ javaScrips/partitures.js
@@ -96,4 +96,8 @@ function crearTaula() {
     container.appendChild(taula);
 }
 
+document.getElementById("login-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.open('./login.html', 'Login', 'width=500,height=400,top=' + (screen.height/2 - 200) + ',left=' + (screen.width/2 - 250));
+});
 window.onload = crearTaula;
