8237ac4443ea8532926368dacd936269e669605d
GabrielFf2 <149058439+GabrielFf2@users.noreply.github.com>
2025-05-22 18:43:38 +0200

    falta tota la migracio del piano

---
+++ tema10/src/views/AndimView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Admin</h1>
+    <p>Aquesta és la part privada.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminView',
+};
+</script>
---
+++ tema10/src/views/PianoView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Piano</h1>
+    <p>Aquesta és la part pública.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PianoView',
+};
+</script>
