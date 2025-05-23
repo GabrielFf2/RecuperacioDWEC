3254cf8dd59bb73e554f675cfb7a7a1e2d33a932
GabrielFf2 <149058439+GabrielFf2@users.noreply.github.com>
2025-05-23 10:15:06 +0200

    falta tota la migracio del piano

--- services/VideosService.js
+++ services/VideosService.js
@@ -2,8 +2,8 @@
 
 export const VideosService = {
     getVideos: () => [
-        { id: 1, src: "./imgs/3013010-uhd_3840_2160_25fps.mp4" },
-        { id: 2, src: "./imgs/3013010-uhd_3840_2160_25fps.mp4" },
-        { id: 3, src: "./imgs/3013010-uhd_3840_2160_25fps.mp4" },
+        { id: 1, src: "./imgs/134486-759714562_small.mp4" },
+        { id: 2, src: "./imgs/134486-759714562_small.mp4" },
+        { id: 3, src: "./imgs/134486-759714562_small.mp4" },
     ],
 };
